d350262cc95090c629ebce6a04fb03be2822a418
Zeeshan Ahmed <za1423@ic.ac.uk>
2023-10-26 14:02:50 +0100

    New files

---
+++ api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_name = request.form.get("name")
+    input_email = request.form.get("email")
+    input_message = request.form.get("message")
+    return render_template(
+        "form.html", name=input_name, email=input_email, message=input_message
+    )
